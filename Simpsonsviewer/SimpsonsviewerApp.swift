0937adc4dd9b13785b17e43290e79e9e91cbaf68
mederland <mederland@gmail.com>
2023-04-25 14:37:22 -0500

    Initial Commit

---
+++ Simpsonsviewer/SimpsonsviewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpsonsviewerApp.swift
+//  Simpsonsviewer
+//
+//  Created by Meder iZimov on 4/25/23.
+//
+
+import SwiftUI
+
+@main
+struct SimpsonsviewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
